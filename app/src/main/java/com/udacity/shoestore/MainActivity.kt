556a371e7b7f1be61e9b369e948e18faf7768455
Archangel354 <rduncan354@gmail.com>
2021-07-14 07:54:44 -0400

    added the <layout tag to my fragment_login.xml.  Very Important!!   This was causing .inflate not be recognized and prompting to change DataBinding to dataViewBinding.

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,14 +2,18 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
 }
