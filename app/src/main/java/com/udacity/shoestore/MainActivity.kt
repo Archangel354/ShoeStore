ae2edc1adb3cb01fc523aacf1ea012aa9da73de4
Archangel354 <rduncan354@gmail.com>
2021-07-17 15:27:02 -0400

    added actionBar code to OnCreate in MainActivity.kt Changed activity_main.xml layout from coordinator layout to linearlayout MOST IMPORTANTLY:  I CHANGED THIS LINE: android:theme="@style/AppTheme.NoActionBar" to android:theme="@style/AppTheme" so my app wouldn't crash in Oncreate with the nav stuff added safeargs plugin to build.gradle and changed viewBinding to dataBinding

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,19 +1,57 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
-import androidx.databinding.DataBindingUtil
+import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        Timber.i("actionbar is null")
+       val actionBar: ActionBar? = this.supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setTitle("I hope this works")
+        }
+        else{
+            Timber.i("actionbar is null")
+        }
+        @Suppress("UNUSED_VARIABLE")
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         Timber.plant(Timber.DebugTree())
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val id: Int = item.getItemId()
+//        if (id == android.R.id.home) {
+//            NavUtils.navigateUpFromSameTask(this)
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
+
 }
