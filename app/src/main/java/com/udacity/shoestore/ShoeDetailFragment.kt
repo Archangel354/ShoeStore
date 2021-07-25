13f6f49d6a7d2d17626b0dad86e71b7eadf1d426
Archangel354 <rduncan354@gmail.com>
2021-07-24 22:56:24 -0400

    updated ShoeStoreViewModel.kt created data class Shoe (name, company, size, description) added binding: FragmentShoeDetailBinding added <layout> tag to fragment_shoe_detail.xml

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -1,11 +1,14 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
+import android.util.Log.i
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 import com.udacity.shoestore.models.ShoeStoreViewModel
 import timber.log.Timber
 
@@ -13,15 +16,25 @@ import timber.log.Timber
 class ShoeDetailFragment : Fragment() {
 
     private lateinit var viewModel: ShoeStoreViewModel
+    private lateinit var binding: FragmentShoeDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
+        binding= DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_shoe_detail,
+            container,
+            false
+        )
+
         Timber.i("Called ViewModelProvider()")
         viewModel = ViewModelProvider(this).get(ShoeStoreViewModel::class.java)
+        var shoe1 = Shoe("boat show", "Crocs", 12, "wear outside")
+        Timber.i("shoe1 is: $shoe1")
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_detail, container, false)
+        return binding.root
+
     }
 
 }
--- app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
@@ -1,5 +1,7 @@
 package com.udacity.shoestore.models
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
@@ -7,6 +9,11 @@ class ShoeStoreViewModel: ViewModel() {
     init {
         Timber.i("ShoeStoreModel created!")
     }
+
+    private val shoesList = mutableListOf<Shoe>()
+    private val _shoeListLiveData = MutableLiveData<List<Shoe>>()
+    val shoeListLiveData: LiveData<List<Shoe>>
+        get() = _shoeListLiveData
 
     override fun onCleared() {
         super.onCleared()
