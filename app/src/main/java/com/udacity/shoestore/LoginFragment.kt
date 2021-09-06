2e9bc7d683db7293aad2d624fe5fafa56063b83f
Archangel354 <rduncan354@gmail.com>
2021-09-05 21:39:39 -0400

    changed popUpTo="@id/shoeListFragment" in navigation.xml so that backstack is "cleared" and I won't be stuck in an infinite loop.  Yay me.

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,18 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
+
+        FragmentManager.POP_BACK_STACK_INCLUSIVE
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
         binding.createNewUserButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment))
         binding.loginButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment))
 
+
+        
+
         return binding.root
     }
+
+
 
 }
